29832065d554acb6d0f6c9c30655c95d2cd42391
aniabrakowska <brakowska.anna@gmail.com>
2019-04-26 15:28:24 +0200

    checked item - change color

--- todolist.js
+++ todolist.js
@@ -1,6 +1,27 @@
 
-window.onload = function(){
 
+
+function addCheck(){
+	var checks = document.getElementsByClassName("fa-check-circle");
+	/*Pętla przechodzi po wszystkich elementach kolekcji 
+	dodaje im nasłuchiwacz zdarzenia click
+	jeśli element zostanie kliknięty to sprawdza czy posiada klase check-item
+	jeśli nie posiada to ja dodaje a jeśli posiada to ja usuwa */
+	for (var check of checks) {
+		check.addEventListener("click", function(){
+
+			if(check.parentNode.classList.contains('check-item') === false)
+			check.parentNode.classList.add('check-item');
+			else check.parentNode.classList.remove('check-item');
+
+		} );
+	    //check.style.color = "red";
+	    console.log(check.parentNode);
+	};
+
+};
+window.onload = function(){
+	
 	var listItem = document.querySelector("ul");
 	var addItem = document.getElementById("add-item");
 
@@ -8,23 +29,27 @@ window.onload = function(){
 	// i dodanie nowego elementy <li>
 	addItem.onclick = function(){
 		var newItem = document.createElement("li");
+		//newitem.classList.add("");
 		var textItem = document.getElementById("text-item");
 		newItem.innerHTML = '<i class="far fa-check-circle"> </i>' + '<p>' + textItem.value + '</p>' + '<div><i class="far fa-edit"></i><i class="far fa-trash-alt"></i></div>';
 
-		var item = document.querySelector('.item');
 		var message = document.getElementById('message');
 
-		if ((textItem.value === "") || (textItem.value === " ")) {
+		if (textItem.value === "" || textItem.value === " ") {
 			message.style.display = "block";
 			message.textContent = "Puste pole!";
 		}
 		else{
 			listItem.appendChild(newItem);
 			message.style.display = "none";
+			addCheck();
 		}
 		
-		console.log(item);
-		
 	};
-
+	
+	
 };
+
+
+
+
